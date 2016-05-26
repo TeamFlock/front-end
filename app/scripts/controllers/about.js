fa26f21286415dae4a3184132794f3a5e5a5d719
Angie Vo <adv3ks@virginia.edu>
2016-05-26 16:55:06 -0400

    angular generator boilerplate

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flockApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the flockApp
+ */
+angular.module('flockApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flockApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the flockApp
+ */
+angular.module('flockApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
