a110946ab8386108ee653eaeb7d891e3d5a4925c
Angie Vo <adv3ks@virginia.edu>
2016-05-27 01:13:12 -0400

    donut chart

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -9,6 +9,34 @@
  */
 angular.module('flockApp')
   .controller('AboutCtrl', function () {
+
+	var ctx = document.getElementById("myChart");
+	var data = {
+	  labels: [
+	      "Red",
+	      "Green",
+	      "Yellow"
+	  ],
+	  datasets: [
+	      {
+	          data: [300, 50, 100],
+	          backgroundColor: [
+	              "#FF6384",
+	              "#36A2EB",
+	              "#FFCE56"
+	          ],
+	          hoverBackgroundColor: [
+	              "#FF6384",
+	              "#36A2EB",
+	              "#FFCE56"
+	          ]
+	      }]
+	};
+	var myDoughnutChart = new Chart(ctx, {
+	type: 'doughnut',
+	data: data
+	});
+	  
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
